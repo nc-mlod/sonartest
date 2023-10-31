2dc0703db185f55a922bd3258cad3d48a2b5299a
aleksandermlodziejewski <mlod@netcompany.com>
2023-10-31 11:10:32 +0100

    fix text

--- src/main/java/Controller/HelloWorldController.java
+++ src/main/java/Controller/HelloWorldController.java
@@ -12,6 +12,6 @@ public class HelloWorldController {
 
     @GetMapping
     public ResponseEntity<String> getHelloWorldText(){
-        return ResponseEntity.status(HttpStatus.OK).body("Hello World");
+        return ResponseEntity.status(HttpStatus.OK).body("Hello World to my controller");
     }
 }
